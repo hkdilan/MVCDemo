a7d2cc282a726978662df3e8db9e7f1f01cfd3ff
Dilan <hkdilan@gmail.com>
2014-03-21 16:37:30 +0530

    first
    this is first

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Detail(int id)
+        {
+            EmployeeContext empContext = new EmployeeContext();
+            Employee employee = empContext.Employees.Single(emp => emp.EmployeeId == id);
+
+            return View(employee);
+        }
+
+    }
+}
